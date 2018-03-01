e378e3d9bcefe0085dea3e58101202611a0bdfb5
E Rasche <hxr@hx42.org>
2018-03-01 14:30:27 +0100

    check links during build.

--- .build.sh
+++ .build.sh
@@ -9,4 +9,8 @@ gem --version
 gem install bundler
 
 bundle install
+# Check internal links
+bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --disable-external
+# Check external
+#bundle exec htmlproofer ./_site --check-html --allow-hash-href --assume-extension --external-only
 bundle exec jekyll build
