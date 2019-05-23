fce03e49797543af2799d321b5bb1729dd0d8c47
Helena Rasche <hxr@hx42.org>
2019-05-23 11:59:21 +0200

    update publisher

--- .publish.sh
+++ .publish.sh
@@ -2,13 +2,14 @@
 set -ex
 
 TMPREPO=$(mktemp -d);
-git clone --quiet git@github.com:usegalaxy-eu/usegalaxy-eu.github.io.git $TMPREPO;
-mv $TMPREPO/.git _site/;
+git clone --quiet git@github.com:usegalaxy-eu/usegalaxy-eu.github.io.git "$TMPREPO";
+mv "$TMPREPO/.git" _site/;
 cp Gemfile Gemfile.lock _site;
 cp readme-deployed.md _site/README.md;
-rm -rf $TMPREPO
+rm -rf "$TMPREPO"
 cd _site
-git add --all *
+touch .nojekyll
+git add --all -- .nojekyll *
 git config --local user.name "UseGalaxy.EU Build Bot"
 git config --local user.email "jenkins@usegalaxy.eu"
 
