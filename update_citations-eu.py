9ce59acd031a67d4d93976110192c6fd40b1e8e2
Beatriz Serrano-Solano <beatrizserrano@users.noreply.github.com>
2021-02-08 13:07:56 +0100

    get rid of 100 limit pubs from zotero

--- update_citations-eu.py
+++ update_citations-eu.py
@@ -4,7 +4,7 @@ from pyzotero import zotero
 import bibtexparser
 zot = zotero.Zotero('1732893', 'group')
 zot.add_parameters(style='mla', format='bibtex', linkwrap="1", tag=">UseGalaxy.eu")
-items = zot.top()
+items = zot.everything(zot.top())
 
 with open('_bibliography/citations-eu.bib', 'w') as bibtex_file:
     bibtexparser.dump(items, bibtex_file)
